340efd746af558b1b7f5807c562718d6488544d0
cjohnston <cjohnston@undergroundelephant.com>
2016-03-08 18:43:48 -0800

    first commit

---
+++ bernie-sandersdotorg/data/memory.js
@@ -0,0 +1,94 @@
+var candidata = {
+
+    Bernie_Sanders: {
+        images: [
+            'http://media.salon.com/2015/05/AP77174442780-1280x960.jpg',
+            'http://fpif.org/wp-content/uploads/2015/09/17324932002_8e3d3e30de_b.jpg',
+            'http://s1.ibtimes.com/sites/www.ibtimes.com/files/styles/embed-md/public/2015/07/21/bernie-sanders.jpg'
+        ],
+        quotes: []
+
+    },
+
+    Hilary_Clinton: {
+        images:[
+            'http://www.newyorker.com/wp-content/uploads/2015/04/Double-Take-Hillary-Clinton-A-Reading-List-1200.jpg',
+            'http://i.telegraph.co.uk/multimedia/archive/02831/Clinton1web_2831249b.jpg',
+            'http://a.abcnews.go.com/images/Politics/GTY_hillary_clinton_kab_150508_16x9_992.jpg'
+        ],
+        quotes:[]
+
+    },
+
+    Roque_De_La_Fuente: {
+        images:[
+            'http://www.eluniversal.com.mx/sites/default/files/styles/f03-651x400/public/2015/11/12/1023_roque.jpg?itok=i1T0rnlF',
+            'http://global.fncstatic.com/static/managed/img/fn-latino/politics/De%20La%20Fuente.jpg',
+            'http://elheraldoslp.com.mx/wp-content/uploads/2015/12/Trump1.jpg'
+        ],
+        quotes:[]
+    },
+
+    Donald_Trump: {
+        images: [
+            'http://static6.businessinsider.com/image/55918b77ecad04a3465a0a63/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg',
+            'http://www.thebostoncalendar.com/system/events/photos/000/055/394/original/donald-trumps-hair_zpswqc4ev8g.jpg?1451575505',
+            'http://www.slate.com/content/dam/slate/blogs/the_slatest/2015/07/09/donald_trump_gop_debate_did_the_republican_national_committee_just_find/464641004-tycoon-donald-trump-speaks-at-the-annual-conservative.jpg.CROP.promo-xlarge2.jpg'
+        ],
+        quotes: [
+            {
+                top: 'The point is,',
+                bottom: 'you can never be too greedy'
+            },
+            {
+                top: 'She’s not giving me 100 percent. She’s giving me 84 percent,',
+                bottom: 'and 16 percent is going towards taking care of children.'
+            },
+            {
+                top:'You know, it really doesn’t matter what the media write',
+                bottom:'as long as you’ve got a young, and beautiful, piece of ass.'
+            },
+
+            {   top:'One of they key problems today is that politics is such a disgrace.',
+                bottom:'Good people don’t go into government'
+            },
+            {
+                top: 'It’s freezing and snowing in New York',
+                bottom: '– we need global warming!'
+            },
+            {   top:'The beauty of me',
+                bottom:'is that I’m very rich'
+            }
+
+        ]
+
+    }
+    ,
+
+    Ted_Cruz: {
+        images:[
+            'http://media.salon.com/2016/01/ted-cruz.jpg',
+            'http://poorrichardsnews.com/wp-content/uploads/2016/01/Cruz.jpg',
+            'http://images.en.yibada.com/data/images/full/26001/u-s-senator-ted-cruz-r-tx.jpg'
+        ],
+        quotes:[]
+    },
+
+    John_Kasich: {
+        images:[
+            'https://cmgddnohiopolitics.files.wordpress.com/2015/08/john-kasich.jpg',
+            'http://c1.nrostatic.com/sites/default/files/uploaded/john-kasich-court-has-ruled-on-gay-marriage-r.jpg',
+            'http://si.wsj.net/public/resources/images/BN-MC906_debate_G_20160114213754.jpg'
+        ],
+        quotes:[]
+    },
+
+    Marco_Rubio: {
+        images:[
+            'http://cdn.redalertpolitics.com/files/2015/09/Rubio.jpg',
+            'http://www.motherjones.com/files/imagecache/master-image-main/rubio_perplexed_master_0.jpg',
+            'http://www.bilerico.com/images/marco-rubio-water.jpg'
+        ],
+        quotes:[]
+    }
+};
